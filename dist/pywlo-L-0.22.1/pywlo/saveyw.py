23a76f304d6675ed456ef6785630118bb9e5a7e0
peter88213 <petert88213@gmail.com>
2020-06-14 18:28:55 +0200

    Prepare v0.22.1 based on PyWriter v1.10.2
    Fix a bug in the ant buildfile not setting the release number in the
    Python scripts.

--- dist/pywlo-L-0.22.1/pywlo/saveyw.py
+++ dist/pywlo-L-0.22.1/pywlo/saveyw.py
@@ -2,7 +2,7 @@
 
 Input file format: html (with visible or invisible chapter and scene tags).
 
-Version @release
+Version 0.22.1
 
 Copyright (c) 2020, peter88213
 For further information see https://github.com/peter88213/PyWriter
