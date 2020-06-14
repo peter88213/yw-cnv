eb960de05f70b471715b4fd47b3a36ea88de66da
peter88213 <petert88213@gmail.com>
2020-06-14 18:10:09 +0200

    Prepare v0.22.0 based on PyWriter v1.10.2
    Add update information.

--- dist/pywlo-L-0.22.0/pywlo/openyw.py
+++ dist/pywlo-L-0.22.0/pywlo/openyw.py
@@ -3,7 +3,7 @@
 Input file format: yWriter
 Output file format: odt (with visible or invisible chapter and scene tags) or csv.
 
-Version 0.21.0
+Version @release
 
 Copyright (c) 2020, peter88213
 For further information see https://github.com/peter88213/PyWriter
