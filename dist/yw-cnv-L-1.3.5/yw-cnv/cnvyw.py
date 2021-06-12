faee9e823b2c25300190754df58a38e55eff5ddd
peter88213 <peter.88213@t-online.de>
2021-06-12 18:04:44 +0200

    Prepare development version v1.3.5 based on PyWriter v3.6.5
    Add "Replace scene dividers" menu entries.

--- dist/yw-cnv-L-1.3.5/yw-cnv/cnvyw.py
+++ dist/yw-cnv-L-1.3.5/yw-cnv/cnvyw.py
@@ -1,6 +1,6 @@
 """Convert yWriter project to odt or ods and vice versa. 
 
-Version 1.3.3
+Version 1.3.5
 
 Copyright (c) 2021 Peter Triesberger
 For further information see https://github.com/peter88213/yw-cnv
