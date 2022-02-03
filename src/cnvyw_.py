f44c9a82179b2c24f2cc8d4f0ca10e3248c7aef0
peter88213 <peter.88213@t-online.de>
2022-02-03 16:50:09 +0100

    Make the requirement for Python 3.6+.

--- src/cnvyw_.py
+++ src/cnvyw_.py
@@ -1,7 +1,7 @@
 """Convert yWriter project to odt or ods and vice versa. 
 
 Version @release
-
+Requires Python 3.6+
 Copyright (c) 2021 Peter Triesberger
 For further information see https://github.com/peter88213/yw-cnv
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
