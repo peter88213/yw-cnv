1ba66ddaa05c9e2ea158bf02bce167ed602a980b
peter88213 <peter.88213@t-online.de>
2023-06-09 12:51:38 +0200

    Update docstring

--- tools/build_cnvyw.py
+++ tools/build_cnvyw.py
@@ -1,8 +1,6 @@
-""" Build python script for the LibreOffice "convert yWriter" script.
+"""Build a Python script for the LibreOffice "convert yWriter" script.
         
-In order to distribute single scripts without dependencies, 
-this script "inlines" all modules imported from the pywriter package.
-
+Copyright (c) 2023 Peter Triesberger
 For further information see https://github.com/peter88213/PyWriter
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
